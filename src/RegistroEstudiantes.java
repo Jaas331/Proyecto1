878af43899ed1e7fe94e8ea81c47c817e5d89f81
Johan Abello <jabellonjaas@gmail.com>
2025-07-08 19:33:52 -0500

    El metodo mail esta completo

--- src/RegistroEstudiantes.java
+++ src/RegistroEstudiantes.java
@@ -4,7 +4,9 @@ public class RegistroEstudiantes {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
+        var salir = false;
         
+        do {
         System.out.print("""
                 --- Sistema de Registro de Estudiantes ---
 
@@ -13,13 +15,62 @@ public class RegistroEstudiantes {
                 3. Calcular promedio de notas del estudiante actual
                 0. Salir
                 
-                seleccione la opción que desea realizar
-                """);
+                seleccione la opción que desea realizar: """);
         var opcion = Integer.parseInt(entrada.nextLine());
 
-        System.out.println(opcion);
+        switch (opcion) {
+            case 1:
+                registroEstudiante();
+                break;
+            case 2:
+                datosEstudiantes();
+                break;
+            case 3:
+                promedioEstudiante();
+                break;
+            case 0:
+                salir = true;
+                break;
+            default:
+                System.out.println("Opción incorrecta");
+                break;
+        }
+
+        System.out.println("\nPrecione ENTER para continuar");
+        entrada.nextLine();
+        limpiarPantalla();
 
 
+        
+    } while (!salir);
+        System.out.println("saliendo del sistema");
+        entrada.close();
+
+    }
+
+
+    // Metodos 
+
+    private static void limpiarPantalla() {
+        System.out.println();
+    }
+
+
+    private static void promedioEstudiante() {
+
+    }
+
+
+
+
+    private static void datosEstudiantes() {
+
+    }
+
+
+
+
+    private static void registroEstudiante() {
 
     }
 
